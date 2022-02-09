2930cd40db54e3e17315e248c76561b128264131
Shashikant Tanti <shashikant.tanti@gmail.com>
2022-02-09 19:45:00 +0530

    remaining code updated

--- SalaryProjection/src/com/knoldus/learning/design/salary/type/CurrencyType.java
+++ SalaryProjection/src/com/knoldus/learning/design/salary/type/CurrencyType.java
@@ -1,5 +1,6 @@
 package com.knoldus.learning.design.salary.type;
 
 public enum CurrencyType {
+    USD,INR,EURO,YEN
 
 }
--- SalaryProjection/src/com/knoldus/learning/design/salary/type/DepartmentType.java
+++ SalaryProjection/src/com/knoldus/learning/design/salary/type/DepartmentType.java
@@ -1,5 +1,6 @@
 package com.knoldus.learning.design.salary.type;
 
 public enum DepartmentType {
+    IT,HR,FINANCE,MANAGEMENT
 
 }
